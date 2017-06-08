c433845197f7f5b51b3a5868ae9b81367519d5a3
Bu Fan <821869798@qq.com>
2017-06-08 10:07:53 +0800

    环境初步

--- js/md.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $.get("markdown/m1.md", function(result){
-        console.log(result);
-        $("#md").html(result);
-        $.getScript("http://strapdownjs.com/v/0.2/strapdown.js");
-    });
-});
